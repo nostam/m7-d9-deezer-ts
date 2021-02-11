169bf84ecfae75f341947c6a79dc451e691d9c22
nostam <matsonho@gmail.com>
2021-02-11 21:46:34 +0100

    track state is always 1 cycle behind selected track

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
+import { Result } from "./types/interfaces";
 
 function App() {
-  const [data, setData]: [any, any] = useState();
-  const handleResult = (data: any) => setData(data);
+  const [data, setData] = useState<Result[]>([]);
+  function handleResult(data: Result[]) {
+    return setData(data);
+  }
   return (
     <Router>
       <NavBar setData={handleResult} />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Container, Card } from "react-bootstrap";
-import { ISearchResult, ITrackId } from "../types/interfaces";
+import { Result, IEventTrackId, Track } from "../types/interfaces";
 import { RouteComponentProps } from "react-router-dom";
 import { searchTrachUrl as url, headers } from "../libs/deezer";
+interface HomeProps extends RouteComponentProps {
+  data: Result[];
+}
+// interface CustomType extends React.MouseEvent {
+//   target: Result;
+// }
 
-type Props = ISearchResult & RouteComponentProps;
-type CustomType = React.MouseEvent<HTMLDivElement> & ITrackId;
-export default function Home({ data }: Props) {
+type CustomType = React.MouseEvent<HTMLDivElement> & IEventTrackId;
+
+export default function Home({ data }: HomeProps) {
   // find unique albums
   // const [unique, setUnique]: [any, any] = useState([]);
   // useEffect(() => {
   //   setUnique(Array.from(new Set([data?.data])));
   // }, [data]);
-  const [track, setTrack] = useState({});
-  const handleTrack = (e: CustomType) => {
-    fetchTrack(e.target.id);
-  };
-  const fetchTrack = async (id: number) => {
+  const [track, setTrack] = useState<Track>();
+  // const handleClick = (fieldName: string) => (e: CustomType) => {
+  //   console.log("clicked id", e.target.id, e.target.offsetParent.id);
+  //   fetchTrack(e.target.id);
+  // };
+  const [trackId, setTrackId] = useState<number>();
+  function handleClick(e: CustomType) {
+    console.log("clicked id", e.target.id, e.target.offsetParent.id);
+
+    const id: number = e.target.id ? e.target.id : e.target.offsetParent.id;
+    // setTrackId(id);
+    fetchTrack(id);
+  }
+  // useEffect(() => {
+  //   fetchTrack(data[0]?.id);
+  // }, []);
+  // useEffect(() => {
+  //   fetchTrack(trackId);
+  // }, [trackId]);
+  async function fetchTrack(id: number) {
     try {
       const res = await fetch(url + id, { headers: headers });
       if (res.ok) {
-        const payload = await res.json();
-        console.log(payload);
+        const payload = (await res.json()) as Track;
         setTrack(payload);
-        console.log(track);
+        console.log("payload", payload.id, "state", track?.id);
       }
     } catch (error) {
       console.log("error", error.message);
     }
-  };
+  }
   return (
     <>
       <Container className="row mx-auto ">
-        {data?.data?.map((entry: any, i: number) => (
+        {data?.slice(0, 6).map((entry: Result) => (
           <Card
-            key={`home${i}`}
+            key={entry.id}
             style={{ width: "18rem" }}
             className="mx-auto my-2"
             id={entry.id}
-            onClick={(e: CustomType) => handleTrack(e)}
+            // onClick={() => setTrackId(entry.id)}
+            onClick={handleClick}
           >
-            <Card.Img src={entry?.album?.cover_medium} />
-            <Card.Title>{entry?.title_short}</Card.Title>
-            <Card.Title>{entry?.artist.name}</Card.Title>
+            <Card.Img src={entry.album.cover_medium} />
+            <Card.Title>{entry.title_short}</Card.Title>
+            <Card.Title>{entry.artist.name}</Card.Title>
           </Card>
         ))}
       </Container>
